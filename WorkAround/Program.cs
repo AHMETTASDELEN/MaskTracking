8c5e9c91257799aed57819ad47801e708c1bc609
ahmet <ahmet@DESKTOP-CNNJCU5>
2024-03-07 13:15:25 +0300

    Add project files.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace WorkAround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Person person1 = new Person();
+            person1.FirsName = "ENGİN";
+            person1.LastName = "DEMİROĞ";
+            person1.DateOfBirthYear = 1234;
+            person1.NationalityIdentity = 1234;
+
+            PttManager pttManager=new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+    }
+}
